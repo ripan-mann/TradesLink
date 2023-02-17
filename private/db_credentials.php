cb5b7777c3e18999a59c2f4a90401751b01911e8
ripan-mann <ripanmann3@gmail.com>
2023-02-16 16:17:11 -0800

    DB Credentials Updated

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,8 @@
 <?php
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "secretpassword");
+define("DB_SERVER", "us-cdbr-east-06.cleardb.net");
+define("DB_USER", "b5ced7ffdf6d51");
+define("DB_PASS", "6c19e93b");
 define("DB_NAME", "tradeslink");
 
 ?>
